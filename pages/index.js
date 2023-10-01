50e3d4dfbc65f9c1cde87e5fe751ebff5718e039
Kaio Gonçalves <eukaio@gmail.com>
2023-10-01 22:44:51 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home(){
-    return <h1>Te amo meu bem. Quando ler vem aqui me dar um beijo.</h1>
+    return <><h1>Te amo meu bem. Quando ler vem aqui me dar um beijo.</h1><h2>Clique <a href="https://www.mundodasmensagens.com/mensagem/voce-me-traz-a-paz.html">aqui</a> para ver a minha mensagem de amor.</h2></>
+
 }
 
 export default Home;
