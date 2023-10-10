775529c5077b808c49059dea67d0b803640e84d4
Kaio Gonçalves <eukaio@gmail.com>
2023-10-10 02:03:41 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
-function Home(){
-    return <><h1>Te amo meu bem. Quando ler vem aqui me dar um beijo.</h1><h2>Clique <a href="https://www.mundodasmensagens.com/mensagem/voce-me-traz-a-paz.html">aqui</a> para ver a minha mensagem de amor.</h2></>
-
+function Home() {
+  return (
+    <>
+      <h1>Te amo meu bem. Quando ler vem aqui me dar um beijo.</h1>
+      <h2>
+        Clique{" "}
+        <a href="https://www.mundodasmensagens.com/mensagem/voce-me-traz-a-paz.html">
+          aqui
+        </a>{" "}
+        para ver a minha mensagem de amor.
+      </h2>
+    </>
+  );
 }
 
 export default Home;
