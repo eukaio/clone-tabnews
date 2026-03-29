05b5f9cb0d5dac99310f113d90c44a811f3d1891
Kaio Gonçalves <eukaio@gmail.com>
2026-03-29 22:27:17 +0000

    refactor: remove `async` from `setSessionCookie()` and `clearSessionCookie()`

--- infra/controller.js
+++ infra/controller.js
@@ -40,7 +40,7 @@ function onErrorHandler(error, request, response) {
   response.status(publicErrorObject.statusCode).json(publicErrorObject);
 }
 
-async function setSessionCookie(sessionToken, response) {
+function setSessionCookie(sessionToken, response) {
   const setCookie = cookie.serialize("session_id", sessionToken, {
     path: "/",
     maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
@@ -84,7 +84,7 @@ async function injectAnonymousOrUser(request, response, next) {
   }
 }
 
-async function clearSessionCookie(response) {
+function clearSessionCookie(response) {
   const setCookie = cookie.serialize("session_id", "invalid", {
     path: "/",
     maxAge: -1,
