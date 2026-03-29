41c11814e9cfce35ef8f15ee7bf3848ca344baf7
Kaio Gonçalves <eukaio@gmail.com>
2026-03-29 23:35:12 +0000

    feat: add `sameSite=Lax` to `controller.setSessionCookie()`

--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -71,6 +71,7 @@ describe("GET /api/v1/user", () => {
         maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
         path: "/",
         httpOnly: true,
+        sameSite: "Lax",
       });
     });
 
@@ -133,6 +134,7 @@ describe("GET /api/v1/user", () => {
         maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
         path: "/",
         httpOnly: true,
+        sameSite: "Lax",
       });
     });
 
